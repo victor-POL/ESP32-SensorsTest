9075ac9a838001a8f499e3779b8e18004b72fb0f
Victor <99147005+victor-POL@users.noreply.github.com>
2023-09-16 17:37:06 -0300

    Testing lcd

--- src/main.ino
+++ src/main.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include <Keypad.h>
+#include "rgb_lcd.h"
 
 // Buzzer
 #define STATUS_KEY_SOUND 10
@@ -85,6 +86,9 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, columnPins, ROWS_KEYPAD, COLS_
 #define DOOR_SENSOR_TRIGGER_PIN 33
 #define DOOR_SENSOR_ECHO_PIN 35
 
+// LCD
+rgb_lcd lcd;
+
 void setup()
 {
     Serial.begin(115200);
@@ -101,12 +105,14 @@ void setup()
 
     // setupUltrasonicEntrance();
 
-    setupUltrasonicDoor();
+    // setupUltrasonicDoor();
+
+    setupLCD();
 }
 
 void loop()
 {
-    testUltrasonicDoor();
+    testLCD();
 }
 
 // Buzzer
@@ -347,4 +353,37 @@ void testUltrasonicDoor()
     Serial.print("Puerta: ");
     Serial.println(distance);
     delay(2000);
+}
+
+// LCD
+void setupLCD()
+{
+    lcd.begin(16, 2);
+}
+
+void testLCD()
+{
+    lcd.setRGB(0, 150, 0);
+    // set the cursor to (0,0):
+    lcd.setCursor(0, 0);
+    // print from 0 to 9:
+    for (int thisChar = 0; thisChar < 10; thisChar++) {
+        lcd.print(thisChar);
+        delay(500);
+    }
+
+    // set the cursor to (16,1):
+    lcd.setCursor(16, 1);
+    // set the display to automatically scroll:
+    lcd.autoscroll();
+    // print from 0 to 9:
+    for (int thisChar = 0; thisChar < 10; thisChar++) {
+        lcd.print(thisChar);
+        delay(500);
+    }
+    // turn off automatic scrolling
+    lcd.noAutoscroll();
+
+    // clear screen for the next loop:
+    lcd.clear();
 }
