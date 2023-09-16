e24ff9ce394ed4ab8f665b549293e3a5419c17ca
Victor <99147005+victor-POL@users.noreply.github.com>
2023-09-15 22:41:33 -0300

    Testing buzzer

---
+++ src/main.ino
@@ -0,0 +1,65 @@
+#include <Arduino.h>
+
+// Buzzer
+#define BUZZER_PIN 27
+
+#define BUZZER_CHANNEL 4
+#define BUZZER_RESOLUTION 8
+#define BUZZER_FREQUENCY 2000
+
+#define BUZZER_SUCCESS_FREQ 800
+#define BUZZER_FAIL_FREQ 200
+#define BUZZER_KEY_FREQ 500
+
+#define BUZZER_KEY_DURATION 100
+#define BUZZER_SUCCESS_DURATION 3000
+#define BUZZER_FAIL_DURATION 3000
+
+void setup()
+{
+    Serial.begin(115200);
+    
+    setupBuzzer();
+}
+
+void loop()
+{
+    testBuzzer();
+}
+
+void setupBuzzer()
+{
+    ledcSetup(BUZZER_CHANNEL, BUZZER_FREQUENCY, BUZZER_RESOLUTION);
+    ledcAttachPin(BUZZER_PIN, BUZZER_CHANNEL);
+}
+
+void testBuzzerKeySoundAndDuration()
+{
+    ledcWriteTone(BUZZER_CHANNEL, BUZZER_KEY_FREQ);
+    delay(BUZZER_KEY_DURATION);
+    ledcWriteTone(BUZZER_CHANNEL, 0);
+}
+
+void testBuzzerSuccesSoundAndDuration()
+{
+    ledcWriteTone(BUZZER_CHANNEL, BUZZER_SUCCESS_FREQ);
+    delay(BUZZER_SUCCESS_DURATION);
+    ledcWriteTone(BUZZER_CHANNEL, 0);
+}
+
+void testBuzzerFailSoundAndDuration()
+{
+    ledcWriteTone(BUZZER_CHANNEL, BUZZER_FAIL_FREQ);
+    delay(BUZZER_FAIL_DURATION);
+    ledcWriteTone(BUZZER_CHANNEL, 0);
+}
+
+void testBuzzer()
+{
+    testBuzzerKeySoundAndDuration();
+    delay(2000);
+    testBuzzerSuccesSoundAndDuration();
+    delay(2000);
+    testBuzzerFailSoundAndDuration();
+    delay(2000);
+}
