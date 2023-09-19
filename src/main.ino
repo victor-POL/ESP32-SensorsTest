0350ea57adb97a914fc9fed7f1e887acf1188b71
Victor <99147005+victor-POL@users.noreply.github.com>
2023-09-18 22:59:04 -0300

    Testing NFC

--- src/main.ino
+++ src/main.ino
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include <Keypad.h>
+#include <Wire.h>
 #include "rgb_lcd.h"
+#include <SPI.h>
+#include <MFRC522.h>
 
 // Buzzer
 #define STATUS_KEY_SOUND 10
@@ -89,6 +92,12 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, columnPins, ROWS_KEYPAD, COLS_
 // LCD
 rgb_lcd lcd;
 
+// NFC
+#define SS_PIN  5  // ESP32 pin GPIO5 
+#define RST_PIN 27 // ESP32 pin GPIO27 
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
 void setup()
 {
     Serial.begin(115200);
@@ -107,12 +116,14 @@ void setup()
 
     // setupUltrasonicDoor();
 
-    setupLCD();
+    // setupLCD();
+
+    setupNFC();
 }
 
 void loop()
 {
-    testLCD();
+    testNFC();
 }
 
 // Buzzer
@@ -386,4 +397,35 @@ void testLCD()
 
     // clear screen for the next loop:
     lcd.clear();
+}
+
+// NFC
+void setupNFC()
+{
+    SPI.begin(); // init SPI bus
+    rfid.PCD_Init(); // init MFRC522
+
+    Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
+}
+
+void testNFC()
+{
+    if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
 }
